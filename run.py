e97729fe684b5f83b2b045cbf6dc1a4afafc1fcf
Boris Teodorovich <boris@degirum.com>
2025-11-13 02:38:14 -0800

    Moving async stuff into tiled codegen

--- run.py
+++ run.py
@@ -38,6 +38,8 @@ class StridedShape:
         acc *= dim
     self.strides = [0 if s == 1 else st for s, st in zip(shape, strides)]
     assert len(shape) == len(strides)
+  def __getitem__(self, i):
+    return self.shape[i]
   def __repr__(self):
     return f"StridedShape(shape={self.shape}, strides={self.strides})"
   def reshape(self, newshape):
@@ -106,8 +108,8 @@ class Fragments:
 # col =  (threadID_in_group * 2) + (i & 0x1)          for ai where i <  4
 # (threadID_in_group * 2) + (i & 0x1) + 8      for ai where i >= 4
 
-#M, N, K = 4096, 4096, 4096
-M, N, K = 16, 8, 16
+M, N, K = 4096, 4096, 4096
+#M, N, K = 16, 8, 16
 
 A_shape = StridedShape([M, K])
 B_shape = StridedShape([K, N])
@@ -123,62 +125,91 @@ TILES_N = int(math.ceil(N / Nmma))
 TILES_K = int(math.ceil(K / Kmma))
 NBLOCKS = TILES_M * TILES_N * TILES_K
 
+A_sbuf_shape = StridedShape([Mmma, Kmma])
+B_sbuf_shape = StridedShape([Kmma, Nmma])
+
 
 # TODO: Clean up syntax to specify these
 a_locs = [('by', f'{(i//2%2)*Mmma//2}+(tid/4)', 'i', f'{(i%2)+(i//4%2)*Kmma//2}+(tid%4)*2') for i in range(8)]
-a_frags = Fragments('A', A_shape.reshape([TILES_M, Mmma, TILES_K, Kmma]), 'half', 2, 8, a_locs)
+a_frags = Fragments('s_a', A_sbuf_shape.reshape([A_sbuf_shape[0]//Mmma, Mmma, A_sbuf_shape[1]//Kmma, Kmma]), 'half', 2, 8, a_locs)
 
 b_locs = [('i', f'{(i%2)+(i//2)*Kmma//2}+(tid%4)*2', 'bx', 'tid/4') for i in range(4)]
-b_frags = Fragments('B', B_shape.reshape([TILES_K, Kmma, TILES_N, Nmma]), 'half', 2, 4, b_locs)
+b_frags = Fragments('s_b', B_sbuf_shape.reshape([B_sbuf_shape[0]//Kmma, Kmma, B_sbuf_shape[1]//Nmma, Nmma]), 'half', 2, 4, b_locs)
 
 d_locs = [('by', f'{(i//2)*Mmma//2}+(tid/4)', 'bx', f'{i%2}+(tid%4)*2') for i in range(4)]
-d_frags = Fragments('D', C_shape.reshape([TILES_M, Mmma, TILES_N, Nmma]), 'float', 4, 4, d_locs)
+d_frags = Fragments('d', C_shape.reshape([TILES_M, Mmma, TILES_N, Nmma]), 'float', 4, 4, d_locs)
 
 
 # ======================================================================================
 # CUDA kernel for FP16 matrix multiplication with FP32 accumulation
 # ======================================================================================
-def code_decl(mat_frags, val=None):
-  return f'{mat_frags.dtype} {mat_frags.name.lower()}_frag[{mat_frags.nregs}]{f" = {val}" if val is not None else ""};'
+def code_decl(var, val=None):
+  return f'{var.dtype} {var.frag_name}[{var.nregs}]{f" = {val}" if val is not None else ""};'
 def code_load_v2(mat_frags):
-  return '      '.join((f'{mat_frags.name.lower()}_frag[{i}] = {mat_frags.name}[{loc}];\n'
-      for i, loc in enumerate(mat_frags.locs)))
-
+  return '    '.join((f'{mat_frags.frag_name}[{i}] = {mat_frags.name}[{loc}];\n' for i, loc in enumerate(mat_frags.locs)))
+def code_store_v2(mat_frags):
+  return '\n  '.join((f'{mat_frags.name}[{loc}] = {mat_frags.frag_name}[{i}];' for i, loc in enumerate(mat_frags.locs)))
 def code_exec(a, b, c_d):
   return f'''
-      // --- MMA PTX ---
-      unsigned int* {a.int_name} = reinterpret_cast<unsigned int *>({a.frag_name});
-      unsigned int* {b.int_name} = reinterpret_cast<unsigned int *>({b.frag_name});
-      unsigned int* {c_d.int_name} = reinterpret_cast<unsigned int *>({c_d.frag_name});
-      asm volatile(
-        "mma.sync.aligned.m16n8k16.row.col.f32.f16.f16.f32 {{%0,%1,%2,%3}},{{%4,%5,%6,%7}},{{%8,%9}},{{%0,%1,%2,%3}};\\n"
-        : {",".join(f"\"+r\"({c_d.int_name}[{i}])" for i in range(c_d.nint32))}
-        : {",".join(f"\"r\"({a.int_name}[{i}])" for i in range(a.nint32))}
-        , {",".join(f"\"r\"({b.int_name}[{i}])" for i in range(b.nint32))}
-      );
-'''
-
-def code_store_v2(mat_frags):
+    // --- MMA PTX ---
+    unsigned int* {a.int_name} = reinterpret_cast<unsigned int *>({a.frag_name});
+    unsigned int* {b.int_name} = reinterpret_cast<unsigned int *>({b.frag_name});
+    unsigned int* {c_d.int_name} = reinterpret_cast<unsigned int *>({c_d.frag_name});
+    asm volatile(
+      "mma.sync.aligned.m16n8k16.row.col.f32.f16.f16.f32 {{%0,%1,%2,%3}},{{%4,%5,%6,%7}},{{%8,%9}},{{%0,%1,%2,%3}};\\n"
+      : {",".join(f"\"+r\"({c_d.int_name}[{i}])" for i in range(c_d.nint32))}
+      : {",".join(f"\"r\"({a.int_name}[{i}])" for i in range(a.nint32))}
+      , {",".join(f"\"r\"({b.int_name}[{i}])" for i in range(b.nint32))}
+    );'''
+def code_async_load_g2s(*, g, s, nbytes):
   return f'''
-    {'\n    '.join((f'{mat_frags.name}[{loc}] = {mat_frags.name.lower()}_frag[{i}];' for i, loc in enumerate(mat_frags.locs)))}
+    asm volatile("cp.async.ca.shared.global [%0],[%1],{nbytes};\\n"
+      :
+      : "r"((int)({s}))
+      , "l"({g})
+    );'''
+def code_commit():
+    return 'asm volatile("cp.async.commit_group;\\n" ::);'
+def code_sync():
+  return f'''
+    asm volatile("cp.async.wait_group 0;\\n" ::);
+    __syncthreads();
   '''
 
-code = f'''extern "C"
-__global__ void matmul_fp16_fp32(const half* A, const half* B, float* C, float* D) {{
-    unsigned int tid = threadIdx.x;
-    unsigned int bx = blockIdx.x;
-    unsigned int by = blockIdx.y;
-    unsigned int bz = blockIdx.z;
-    {code_decl(a_frags)}
-    {code_decl(b_frags)}
-    {code_decl(d_frags, "{0.0f,0.0f,0.0f,0.0f}")}
 
-    for (int i = 0; i < {TILES_K}; ++i) {{
-      {code_load_v2(a_frags)}
-      {code_load_v2(b_frags)}
-      {code_exec(a_frags, b_frags, d_frags)}
-    }}
-    {code_store_v2(d_frags)}
+code = f'''extern "C"
+__global__ void matmul_fp16_fp32(
+  const half* a,
+  const half* b,
+  float* c,
+  float* d)
+{{
+  unsigned int tid = threadIdx.x;
+  unsigned int bx = blockIdx.x;
+  unsigned int by = blockIdx.y;
+  unsigned int bz = blockIdx.z;
+  {code_decl(a_frags)}
+  {code_decl(b_frags)}
+  {code_decl(d_frags, "{0.0f,0.0f,0.0f,0.0f}")}
+
+  // TODO: Next step - double buf pipeline
+  __shared__ half s_a[{Mmma*Kmma}];
+  __shared__ half s_b[{Kmma*Nmma}];
+  int s_a_addr = __cvta_generic_to_shared(s_a);
+  int s_b_addr = __cvta_generic_to_shared(s_b);
+
+  // Loop over TILES_K
+  for (int i = 0; i < {TILES_K}; ++i) {{
+    {code_async_load_g2s(s="s_a_addr+tid*8*sizeof(half)", g=f"&a[by*{Mmma*K}+i*{Kmma}+(tid/2)*{K}+(tid%2)*8]", nbytes=16)}
+    {code_async_load_g2s(s="s_b_addr+tid*4*sizeof(half)", g=f"&b[i*{Kmma*N}+bx*{Nmma}+(tid/2)*{N}+(tid%2)*4]", nbytes=8)}
+    {code_commit()}
+    {code_sync()}
+    // Done loading tile
+    {code_load_v2(a_frags)}
+    {code_load_v2(b_frags)}
+    {code_exec(a_frags, b_frags, d_frags)}
+  }}
+  {code_store_v2(d_frags)}
 }}
 '''
 
